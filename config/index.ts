e9d26283021c2405467a56465e2cd10847c46656
Ishan Dongol <ishandongol@gmail.com>
2021-10-31 18:14:00 +0545

    ENV Defaults

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 const config = {
-  apiUrl: process.env.NEXT_PUBLIC_BACKEND_URL || '',
-  webSocketUrl: process.env.NEXT_PUBLIC_WS_URL || '',
-  appName: process.env.NEXT_PUBLIC_NAME || '',
+  apiUrl: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://slack-music-bot-rs.herokuapp.com',
+  webSocketUrl: process.env.NEXT_PUBLIC_WS_URL || 'wss://slack-music-bot-rs.herokuapp.com/ws',
+  appName: process.env.NEXT_PUBLIC_NAME || 'Music Playground',
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL || '/slack-music-bot-client'
 };
 
