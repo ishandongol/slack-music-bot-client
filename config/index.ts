afcc6b96d0fcd546d13a746ef7be1566ad797609
Ishan Dongol <ishandongol@gmail.com>
2021-10-31 17:50:00 +0545

    Base Url

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ const config = {
   apiUrl: process.env.NEXT_PUBLIC_BACKEND_URL || '',
   webSocketUrl: process.env.NEXT_PUBLIC_WS_URL || '',
   appName: process.env.NEXT_PUBLIC_NAME || '',
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || ''
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || '/slack-music-bot-client'
 };
 
 export default config;
